9032ea79f78e2d95c6acaca86cb60c9427a3d548
Andres Castro <ac-uy@users.noreply.github.com>
2026-07-11 10:39:37 +0200

    refactor: use mode property (cool/heat) from ramses_rf v0.57.13

--- custom_components/ramses_cc/climate.py
+++ custom_components/ramses_cc/climate.py
@@ -264,8 +264,8 @@ class RamsesController(RamsesEntity, ClimateEntity):
 
         zone_demand = resolve_async_attr(self, self._device, "zone_demand")
         if zone_demand:
-            cooling = resolve_async_attr(self, self._device, "cooling_active")
-            if cooling is True:
+            mode = resolve_async_attr(self, self._device, "mode")
+            if mode == "cool":
                 return HVACAction.COOLING
             return HVACAction.HEATING
         if zone_demand is not None:
@@ -285,10 +285,10 @@ class RamsesController(RamsesEntity, ClimateEntity):
                 return HVACMode.OFF
             if system_mode[SZ_SYSTEM_MODE] == SystemMode.AWAY:
                 return HVACMode.AUTO  # users can't adjust setpoints away
-        cooling = resolve_async_attr(self, self._device, "cooling_active")
-        if cooling is True:
+        mode = resolve_async_attr(self, self._device, "mode")
+        if mode == "cool":
             return HVACMode.COOL
-        if cooling is False:
+        if mode == "heat":
             return HVACMode.HEAT
         return HVACMode.HEAT  # default when no 2D49 received
 
@@ -566,8 +566,8 @@ class RamsesZone(RamsesEntity, ClimateEntity):
 
         zone_demand = resolve_async_attr(self, self._device, "zone_demand")
         if zone_demand:
-            cooling = resolve_async_attr(self, self._device.tcs, "cooling_active")
-            if cooling is True:
+            tcs_mode = resolve_async_attr(self, self._device.tcs, "mode")
+            if tcs_mode == "cool":
                 return HVACAction.COOLING
             return HVACAction.HEATING
         if zone_demand is not None:
@@ -594,10 +594,10 @@ class RamsesZone(RamsesEntity, ClimateEntity):
         config = resolve_async_attr(self, self._device, "config")
         if config and mode[SZ_SETPOINT] <= config["min_temp"]:
             return HVACMode.OFF
-        cooling = resolve_async_attr(self, self._device.tcs, "cooling_active")
-        if cooling is True:
+        tcs_mode = resolve_async_attr(self, self._device.tcs, "mode")
+        if tcs_mode == "cool":
             return HVACMode.COOL
-        if cooling is False:
+        if tcs_mode == "heat":
             return HVACMode.HEAT
         return HVACMode.HEAT  # default when no 2D49 received
 
